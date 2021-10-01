0cee6ef040fc1d5976cbc1be84f20623e6cf4af0
Nguyễn Tấn Phương <37770098+Phuongaz@users.noreply.github.com>
2021-10-01 22:11:40 +0700

    Add files via upload

---
+++ get_donate_api.py
@@ -0,0 +1,34 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from json import dumps
+from flask.ext.jsonpify import jsonify
+
+app = Flask(__name__)
+api = Api(app)
+
+db_connect = create_engine('sqlite:///locmnapthe.db')
+conn = db_connect.connect()
+
+class get_all_players(Resource):
+    def get(self):
+        query = conn.execute("select * from napthe") 
+        return {'players': [i[0] for i in query.cursor.fetchall()]}
+
+class get_by_name(Resource):
+    def get(self, employee_id):
+        query = conn.execute("select * from napthe where player =%d "  %int(employee_id))
+        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        return jsonify(result)
+
+class get_all(Resource):
+    def get(self):
+        query = conn.execute("select player, amount from player;")
+        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        return jsonify(result)
+
+api.add_resource(get_all, '/api/napthe')
+api.add_resource(get_by_name, '/api/napthe/id/<player>')
+api.add_resource(getall, '/api/napthe/all')
+if __name__ == '__main__':
+     app.run()
